49d676a1e68be73d00e52a89ea5bf7e044be8936
Petra Barus <petra.barus@gmail.com>
2012-09-19 03:41:37 +0700

    New Feature: Adding basic setup for database and Travis configuration.

---
+++ protected/config/application/database.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Returns configuration for database.
+ * 
+ * @author Petra Barus <petra.barus@gmail.com>
+ */
+return array(
+        'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+        'emulatePrepare' => true,
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+);
