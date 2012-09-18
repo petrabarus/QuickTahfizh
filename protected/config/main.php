4486fefd697ea607044ecec8c49c111ea10aed57
Petra Barus <petra.barus@gmail.com>
2012-09-19 04:32:47 +0700

    New Feature: Adding user basic model.

--- protected/config/main.php
+++ protected/config/main.php
@@ -8,7 +8,7 @@ define('APP_CONFIG_DIR', dirname(__FILE__) . '/application');
 // CWebApplication properties can be configured here.
 return array(
         'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
-        'name' => 'My Web Application',
+        'name' => 'Quick Tahfizh',
         // preloading 'log' component
         'preload' => array('log'),
         // autoloading model and component classes
@@ -17,15 +17,13 @@ return array(
                 'application.components.*',
         ),
         'modules' => array(
-        // uncomment the following to enable the Gii tool
-        /*
-          'gii'=>array(
-          'class'=>'system.gii.GiiModule',
-          'password'=>'Enter Your Password Here',
-          // If removed, Gii defaults to localhost only. Edit carefully to taste.
-          'ipFilters'=>array('127.0.0.1','::1'),
-          ),
-         */
+                // uncomment the following to enable the Gii tool
+                'gii' => array(
+                        'class' => 'system.gii.GiiModule',
+                        'password' => 'password',
+                        // If removed, Gii defaults to localhost only. Edit carefully to taste.
+                        'ipFilters' => array('127.0.0.1', '::1'),
+                ),
         ),
         // application components
         'components' => array(
