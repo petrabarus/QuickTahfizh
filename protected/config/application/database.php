4dee009efd0955cf52f955809cff81abfa68a182
Petra Barus <petra.barus@gmail.com>
2012-09-19 14:34:01 +0700

    Fixing indentation per standard.

--- protected/config/application/database.php
+++ protected/config/application/database.php
@@ -6,9 +6,9 @@
  * @author Petra Barus <petra.barus@gmail.com>
  */
 return array(
-        'connectionString' => 'mysql:host=localhost;dbname=tahfizh',
-        'emulatePrepare' => true,
-        'username' => 'tahfizh',
-        'password' => 'tahfizh',
-        'charset' => 'utf8',
+  'connectionString' => 'mysql:host=localhost;dbname=tahfizh',
+  'emulatePrepare' => true,
+  'username' => 'tahfizh',
+  'password' => 'tahfizh',
+  'charset' => 'utf8',
 );
--- protected/models/User.php
+++ protected/models/User.php
@@ -13,55 +13,59 @@
  * @author Petra Barus <petra.barus@gmail.com>
  * @package application.models
  */
+class User extends \CActiveRecord
+{
 
-class User extends \CActiveRecord {
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return User the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
 
-        /**
-         * Returns the static model of the specified AR class.
-         * @param string $className active record class name.
-         * @return User the static model class
-         */
-        public static function model($className = __CLASS__) {
-                return parent::model($className);
-        }
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'Users';
+    }
 
-        /**
-         * @return string the associated database table name
-         */
-        public function tableName() {
-                return 'Users';
-        }
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        return array(
+          array('username', 'match', 'pattern' => '/^[a-zA-Z][a-zA-Z0-9_]+$/', 'message' => Yii::t('rules', '{attribute} is invalid. Only alphabet, number, and underscore allowed')),
+          array('username, fullName, email', 'required'),
+          array('username', 'length', 'max' => 64, 'min' => 5),
+          array('email', 'email'),
+        );
+    }
 
-        /**
-         * @return array validation rules for model attributes.
-         */
-        public function rules() {
-                return array(
-                        array('username', 'match', 'pattern' => '/^[a-zA-Z][a-zA-Z0-9_]+$/', 'message' => Yii::t('rules', '{attribute} is invalid. Only alphabet, number, and underscore allowed')),
-                        array('username, fullName, email', 'required'),
-                        array('username', 'length', 'max' => 64, 'min' => 5),
-                        array('email', 'email'),
-                );
-        }
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        return array(
+        );
+    }
 
-        /**
-         * @return array relational rules.
-         */
-        public function relations() {
-                return array(
-                );
-        }
-
-        /**
-         * @return array customized attribute labels (name=>label)
-         */
-        public function attributeLabels() {
-                return array(
-                        'id' => Yii::t('label', 'ID'),
-                        'username' => Yii::t('label', 'Username'),
-                        'fullName' => Yii::t('label', 'Full Name'),
-                        'email' => Yii::t('label', 'Email'),
-                );
-        }
-
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+          'id' => Yii::t('label', 'ID'),
+          'username' => Yii::t('label', 'Username'),
+          'fullName' => Yii::t('label', 'Full Name'),
+          'email' => Yii::t('label', 'Email'),
+        );
+    }
 }
