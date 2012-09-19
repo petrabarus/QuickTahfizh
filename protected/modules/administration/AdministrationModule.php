eb6d17ef335ef2d4475672c714cf87b45e651931
nurcahyo <nurcahyo@nlc.(none)>
2012-09-20 00:39:49 +0700

    reconfig yii theme, defaultcontroller and adding comment on init method

--- protected/modules/administration/AdministrationModule.php
+++ protected/modules/administration/AdministrationModule.php
@@ -10,39 +10,46 @@
 class AdministrationModule extends CWebModule
 {
 
-    /**
-     * Initializes the modules.
-     * 
-     * This method registers required models and components.
-     * This method is called when the module is being created
-     * you may place code here to customize the module or the application
-     * import the module-level models and components
-     */
-    public function init()
-    {
+	/**
+	 * Initializes the modules.
+	 * 
+	 * This method registers required models and components.
+	 * This method set theme and defaultController.
+	 * This method is called when the module is being created
+	 * you may place code here to customize the module or the application
+	 * import the module-level models and components
+	 */
+	public function init()
+	{
 
-        $this->setImport(array(
-          'administration.models.*',
-          'administration.components.*',
-        ));
-    }
+		$this->configure(array(
+			'import' => array(
+				'administration.models.*',
+				'administration.components.*',
+			),
+			'defaultController'=>'Dashboard',
+		));
+		Yii::app()->configure(array(
+			'theme'=>'administration'
+		));
+	}
 
-    /**
-     * The pre-filter for controller actions.
-     * This method is invoked before the currently requested controller action and all its filters
-     * are executed.
-     * 
-     * @param CController $controller the controller
-     * @param CAction $action the action
-     * @return boolean whether the action should be executed.
-     */
-    public function beforeControllerAction($controller, $action)
-    {
-        if (parent::beforeControllerAction($controller, $action))
-        {
-            return true;
-        }
-        else
-            return false;
-    }
+	/**
+	 * The pre-filter for controller actions.
+	 * This method is invoked before the currently requested controller action and all its filters
+	 * are executed.
+	 * 
+	 * @param CController $controller the controller
+	 * @param CAction $action the action
+	 * @return boolean whether the action should be executed.
+	 */
+	public function beforeControllerAction($controller, $action)
+	{
+		if (parent::beforeControllerAction($controller, $action))
+		{
+			return true;
+		}
+		else
+			return false;
+	}
 }
