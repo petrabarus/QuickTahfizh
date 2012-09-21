0c7217d41dce64dcaa3ee6b4778484896d7e6fd8
Petra Barus <petra.barus@gmail.com>
2012-09-21 20:09:28 +0700

    Issue #7. Adding error handler for admin panel.

---
+++ themes/administration/views/administration/error/index.php
@@ -0,0 +1,11 @@
+<?php $this->pageTitle = $title; ?>
+<p class="error-code">
+    <?php echo $code; ?>
+</p>
+<p class="not-found">
+    <strong><?php echo $title; ?></strong>
+</p>
+<div class="clear"></div>
+<div class="content">
+    <?php echo $message; ?>
+</div>
