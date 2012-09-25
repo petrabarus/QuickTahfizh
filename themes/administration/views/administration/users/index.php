c34c9cc207b5248ae79bcbda44f97715bef23949
Petra Barus <petra.barus@gmail.com>
2012-09-25 21:15:52 +0700

    Fixing the grid view's CSS.

--- themes/administration/views/administration/users/index.php
+++ themes/administration/views/administration/users/index.php
@@ -7,7 +7,7 @@
 <div class="row-fluid sortable ui-sortable">		
     <div class="box span12">
         <div data-original-title="" class="box-header well">
-            <h2><i class="icon-user"></i> Members</h2>
+            <h2><i class="icon-user"></i> <?php echo \Yii::t('messages', 'Members'); ?></h2>
             <div class="box-icon">
                 <a class="btn btn-setting btn-round" href="#"><i class="icon-cog"></i></a>
                 <a class="btn btn-minimize btn-round" href="#"><i class="icon-chevron-up"></i></a>
@@ -28,6 +28,7 @@
                 'dataProvider' => $dataProvider,
                 'htmlOptions' => array(
                     'role' => 'grid',
+                    'class' => 'grid grid-table-bordered',
                 )
                 ));
 
