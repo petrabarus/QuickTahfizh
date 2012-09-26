2b60dfee1d58dfdc6a450dd156c1add799c9bc22
Petra Barus <petra.barus@gmail.com>
2012-09-26 08:04:11 +0700

    Fixing console configuration.
    Fixing fixtures.

--- protected/tests/fixtures/Users.php
+++ protected/tests/fixtures/Users.php
@@ -13,17 +13,23 @@ return array(
         'username' => 'username1',
         'fullName' => 'Full Name 1',
         'email' => 'username1@example.com',
+        'password' => sha1('password1'),
+        'createdTime' => date('Y-m-d H:i:s'),
     ),
     array(
         'id' => 2,
         'username' => 'username2',
         'fullName' => 'Full Name 2',
         'email' => 'username1@example.com',
+        'password' => sha1('password2'),
+        'createdTime' => date('Y-m-d H:i:s'),
     ),
     array(
         'id' => 3,
         'username' => 'username3',
         'fullName' => 'Full Name 3',
         'email' => 'username1@example.com',
+        'password' => sha1('password3'),
+        'createdTime' => date('Y-m-d H:i:s'),
     ),
 );
